2690bc7aa23356d3a1570b2d21e1e08cf7293891
Ben Meneghini <bencmeneghini@gmail.com>
2024-05-07 20:59:35 +1200

    Added a preprocessing step, adjusting contrast, to enhance features of the coins in the dataset.

--- image_picker.py
+++ image_picker.py
@@ -7,6 +7,13 @@ def recognise_from_image():
     root = tk.Tk()
     root.withdraw()
     file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.png *.jpg *.jpeg")])
+
+    if not file_path:
+        return 
     
     recognise(file_path)
     root.destroy()
+
+
+def post_process():
+    pass
--- main.py
+++ main.py
@@ -5,15 +5,15 @@ from image_picker import recognise_from_image
 def run():
     root = tk.Tk()
     root.title("NZ Coin Recognition")
-    root.geometry("200x200")
+    root.geometry("300x150")
 
     # Button for capturing live video for real-time recognition
     capture_button = tk.Button(root, text="Real-time Recognition", command=lambda: capture)
-    capture_button.pack(pady=25)
+    capture_button.pack(pady=15)
 
     # Button for recognition with individual images from files
     detect_button = tk.Button(root, text="Detect with Image", command=recognise_from_image)
-    detect_button.pack(pady=25)
+    detect_button.pack(pady=15)
 
     root.mainloop()
 
