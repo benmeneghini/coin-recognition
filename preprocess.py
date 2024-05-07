8071d756395b0d9e1970c6edcc9206407ef802b4
Ben Meneghini <bencmeneghini@gmail.com>
2024-05-07 21:14:21 +1200

    Added initial preprocessing step

--- preprocess.py
+++ preprocess.py
@@ -1,23 +1,43 @@
 import cv2
 import numpy as np
+import os
 
 ALPHA = 1.5 # Contrast control (1.5 increases contrast)
 BETA = 0 # Brightness control (0 has no effect)
 
+OG_DIRECTORY = "datasets/original/"
+PP_DIRECTORY = "datasets/preprocessed/"
+
+
 def adjust_contrast(image_path, alpha=ALPHA, beta=BETA):
     """
-    Adjusts the contrast and brightness of an image.
+    Adjusts the contrast and brightness of an image from an
+    image path and variables: alpha, and beta.
     """
     # Read the image
     image = cv2.imread(image_path)
-
     # Convert the image from uint8 to float to avoid overflow/underflow
     image = image.astype(np.float64)
-
+    
     # Adjust contrast and brightness
     image = image * alpha + beta
-
     # Clip values to stay within the 0-255 range and convert back to uint8
     image = np.clip(image, 0, 255).astype(np.uint8)
 
     return image
+
+
+def preprocess_initial_data():
+    """
+    Initial preprocessing of every image in the dataset from the original
+    data path and saving to new preprocessed directory.    
+    """
+    for filename in os.listdir(OG_DIRECTORY):
+        if filename.endswith(('.png', '.jpg', '.jpeg')):
+            input_image_path = os.path.join(OG_DIRECTORY, filename)
+            output_image_path = os.path.join(PP_DIRECTORY, filename)
+            image = cv2.imread(input_image_path)
+            if image is not None:
+                adjusted_image = adjust_contrast(image)
+                cv2.imwrite(output_image_path, adjusted_image)
+
--- recognition.py
+++ recognition.py
@@ -1,10 +1,21 @@
 import cv2
 from ultralytics import YOLO
+from preprocess import adjust_contrast
 
 def recognise(image_path):
-    image = cv2.imread(image_path)
+    # Preprocess and read the image from the image path.
+    image = adjust_contrast(image_path)
 
     model = YOLO('runs/detect/yolov8n_custom/weights/best.pt')
     res = model.predict(image_path, show=True, imgsz=640)
 
-    pass
+    # Draw bounding boxes and labels on the image
+    for *box, conf, cls in res.xyxy[0]:
+        x1, y1, x2, y2 = map(int, box)
+        label = model.names[int(cls)]
+        cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.putText(image, f'{label} {conf:.2f}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    cv2.imshow("Coin Recognition", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
